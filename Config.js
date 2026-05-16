be966dcfad2fd857dac61e74993f21b8b1424e4c
Radek Pekárek <rpekis@gmail.com>
2026-05-16 23:03:23 +0200

    Release v0.7.1

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.7.0',
+  version: 'v0.7.1',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,13 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.7.1',
+    date: '2026-05-16',
+    changes: [
+      'Opravena viditelnost tlacítek Zrusit/Ulozit v modalu dlazdice – modal-body nyne scrolluje samostatne',
+    ],
+  },
   {
     version: 'v0.7.0',
     date: '2026-05-16',
