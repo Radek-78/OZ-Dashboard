4d1c7f0be123060ff7ed6891be44ab31c9887df8
Radek Pekárek <rpekis@gmail.com>
2026-05-21 17:42:41 +0200

    Release v0.9.1

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.9.0',
+  version: 'v0.9.1',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,17 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.9.1',
+    date: '2026-05-21',
+    changes: [
+      'Vylepšen vzhled přehledu filiálek',
+      'Vyhledávání a statistika přesunuty do hlavičky',
+      'Přidány rychlé filtry LC VT RM',
+      'Přidán hodnotový filtr sloupců ve stylu Excelu',
+      'Tabulka filiálek využívá dostupnou výšku panelu',
+    ],
+  },
   {
     version: 'v0.9.0',
     date: '2026-05-21',
