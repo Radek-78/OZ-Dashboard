a619b7add613dc88645b99932d4ec97f36d61c4b
Radek Pekárek <rpekis@gmail.com>
2026-05-21 20:02:48 +0200

    Release v0.12.0

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.11.0',
+  version: 'v0.12.0',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,16 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.12.0',
+    date: '2026-05-21',
+    changes: [
+      'Záložka Filiálky přejmenována na LC a Filiálky se dvěma přepínatelnými pohledy',
+      'Přidána správa logistických center — vytváření, editace a mazání LC',
+      'Odstraněn duplicitní sloupec abbreviation z přehledu filiálek',
+      'Správa LC využívá existující LOCATIONS infrastrukturu bez zbytečné duplicity dat',
+    ],
+  },
   {
     version: 'v0.11.0',
     date: '2026-05-21',
