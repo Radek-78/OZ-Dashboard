402346f3374955a360c0838d0c4b11c31dfa3a2e
Radek Pekárek <rpekis@gmail.com>
2026-05-22 08:41:55 +0200

    Release v0.14.6

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.14.5',
+  version: 'v0.14.6',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,15 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.14.6',
+    date: '2026-05-22',
+    changes: [
+      'Pridan debug panel Telexu v subaplikaci odpisu',
+      'Diagnostika zobrazuje rozpoznane soubory, hlavicky, sloupce W WW a ukazky PLU',
+      'Doplnen serverovy endpoint getOdpisyDebugData pro ladeni akcnich artiklu',
+    ],
+  },
   {
     version: 'v0.14.5',
     date: '2026-05-22',
