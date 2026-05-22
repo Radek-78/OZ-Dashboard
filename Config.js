757e355e5a0ef3bbbe8a715fb0f523ed5a70fa69
Radek Pekárek <rpekis@gmail.com>
2026-05-22 09:03:35 +0200

    Release v0.14.8

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.14.7',
+  version: 'v0.14.8',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,15 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.14.8',
+    date: '2026-05-22',
+    changes: [
+      'Opraven vyber PLU sloupce v Telexu prednostne podle Art Cislo PLU',
+      'Parser Telexu ignoruje sloupce s nazvem artiklu pri hledani PLU',
+      'Akcni artikly W WW se nyni paruji na ciselne PLU',
+    ],
+  },
   {
     version: 'v0.14.7',
     date: '2026-05-22',
