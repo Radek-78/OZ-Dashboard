956970bc6b1744090ee5c584cc1349a7945aee71
Radek Pekárek <rpekis@gmail.com>
2026-05-23 07:08:47 +0200

    Release v0.18.1

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.18.0',
+  version: 'v0.18.1',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,13 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.18.1',
+    date: '2026-05-23',
+    changes: [
+      'Stavovy tooltip aktualizace odpisu',
+    ],
+  },
   {
     version: 'v0.18.0',
     date: '2026-05-22',
