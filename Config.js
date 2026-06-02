cbdc3a3caea729a634eb92e68fcc45d8bdd30d4f
Radek Pekárek <rpekis@gmail.com>
2026-06-02 14:28:13 +0200

    Release v0.22.0

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.21.1',
+  version: 'v0.22.0',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,15 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.22.0',
+    date: '2026-06-02',
+    changes: [
+      'Pridana hromadna aktivace/deaktivace filialek (checkboxy ve sloupci Cislo + lista akci)',
+      'Neaktivni filialky se beznym uzivatelum ve frontendu nenacitaji',
+      'Deaktivace filialek zustava zachovana i po synchronizaci',
+    ],
+  },
   {
     version: 'v0.21.1',
     date: '2026-06-02',
