e2489497aa78c0d01a6b66558c031807df5cd3e7
Radek Pekárek <rpekis@gmail.com>
2026-06-02 14:59:10 +0200

    Release v0.22.2

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.22.1',
+  version: 'v0.22.2',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,15 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.22.2',
+    date: '2026-06-02',
+    changes: [
+      'Ikony nyni vertikalne vycentrovane',
+      'Checkbox a cislo filialky vedle sebe',
+      'Tlacitka Aktivovat/Deaktivovat maji ikony',
+    ],
+  },
   {
     version: 'v0.22.1',
     date: '2026-06-02',
