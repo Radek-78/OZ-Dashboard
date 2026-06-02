c9f8b927dd86e9045d2f036d364cb37dff06d916
Radek Pekárek <rpekis@gmail.com>
2026-06-02 12:59:20 +0200

    Release v0.20.4

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.20.3',
+  version: 'v0.20.4',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,13 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.20.4',
+    date: '2026-06-02',
+    changes: [
+      'Zpevneno centrovani ikon v dlazdicich modulu',
+    ],
+  },
   {
     version: 'v0.20.3',
     date: '2026-06-02',
