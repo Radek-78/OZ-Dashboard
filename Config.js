344d641c56a80df3983c445b24098eb4841456c5
Radek Pekárek <rpekis@gmail.com>
2026-06-08 10:01:08 +0200

    Release v0.29.5

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.29.4',
+  version: 'v0.29.5',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,13 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.29.5',
+    date: '2026-06-08',
+    changes: [
+      'Řazení RM v rozevíracím seznamu: nejdříve RM odpovídající filtrům abecedně, poté ostatní',
+    ],
+  },
   {
     version: 'v0.29.4',
     date: '2026-06-08',
