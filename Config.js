12ad4094291d1743b3ec5f8cc2bb8506f42ed8b7
Radek Pekárek <rpekis@gmail.com>
2026-06-08 09:14:59 +0200

    Release v0.28.1

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
   appSubtitle: '',
   logoFileId: '18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD',
   logoUrl: 'https://drive.google.com/thumbnail?id=18mu_Lq1F_FqqSZcolMjLwG0aaQDPMdyD&sz=w320',
-  version: 'v0.28.0',
+  version: 'v0.28.1',
   theme: {
     blue: '#0050aa',
     darkBlue: '#002466',
@@ -16,6 +16,13 @@
 };
 
 const APP_CHANGELOG = [
+  {
+    version: 'v0.28.1',
+    date: '2026-06-08',
+    changes: [
+      'Přidáno chybějící oprávnění pro UrlFetchApp (externí požadavky)',
+    ],
+  },
   {
     version: 'v0.28.0',
     date: '2026-06-08',
